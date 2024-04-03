c2d7de1329ee4a66f0b42da93076a2c0f4cf2709
jamelin2023 <julieamelin@hotmail.fr>
2024-04-03 08:55:33 +0200

    modif event 2.0

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -26,44 +26,6 @@ class EventController extends AbstractController
 
     }
 
-
-/*    #[Route('/create', name: 'create_location')]
-    public function createLocation(Request $request, EntityManagerInterface $em): Response
-    {
-        $locationForm = $this->createForm(LocationType::class);
-        $locationForm->handleRequest($request);
-
-        if($locationForm->isSubmitted() && $locationForm->isSubmitted()){
-            $location = $locationForm->getData();
-            $em->persist($location);
-            $em->flush();
-            return $this->redirectToRoute('create_event',['id'=> $location->getId()]);
-
-        }
-        return $this->render('event/locationForm.html.twig', [
-            'locationForm' => $locationForm,
-        ]);
-    }
-
-    #[Route('/event', name: 'create_event')]
-    public function createEvent(Request $request, EntityManagerInterface $em): Response
-    {
-        $event = new Event();
-        $eventForm = $this->createForm(EventType::class, $event);
-        $eventForm->handleRequest($request);
-
-        if($eventForm->isSubmitted() && $eventForm->isSubmitted()){
-            $event = $eventForm->getData();
-            $em->persist($event);
-            $em->flush();
-            return $this->redirectToRoute('list_event');
-        }
-
-        return $this->render('event/eventForm.html.twig', [
-            'eventForm' => $eventForm,
-        ]);
-    }*/
-
     #[Route('/create', name: 'create_event')]
     public function createEvent(Request $request, EntityManagerInterface $em): Response
     {
