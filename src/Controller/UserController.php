533956829aada6f5ce7727a0fb80802a8338848d
adevais2023 <alexdevais@gmail.com>
2024-04-03 11:39:02 +0200

    update user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,5 +22,46 @@ class UserController extends AbstractController
         ]);
     }
 
-    // l'utilisateur peut modifier son profil
+    //création d'une route de page de détail du user
+    #[Route('/user/detail/{id}', name: 'app_detail')]
+    public function detail(EntityManagerInterface $em, int $id): Response
+    {
+        $user = $em->getRepository(User::class)->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+        return $this->render('user/detail.html.twig', [
+            'id' => $id,
+            'user' => $user
+        ]);
+    }
+
+    // l'utilisateur peut modifier son profil (en récupérant son id)
+    #[Route('/user/update/{id}', name: 'app_update')]
+    public function update(Request $request, EntityManagerInterface $em, int $id): Response
+    {
+        $user = $em->getRepository(User::class)->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Profile updated successfully');
+
+            return $this->redirectToRoute('app_detail', ['id' => $user->getId()]);
+        }
+
+        return $this->render('user/update.html.twig', [
+            'updateForm' => $form->createView(),
+        ]);
+
+    }
 }
+
+
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -37,6 +38,7 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->getEntityManager()->persist($user);
         $this->getEntityManager()->flush();
     }
+
 
     //    /**
     //     * @return User[] Returns an array of User objects
